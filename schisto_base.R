88df118c4c3c56833908e5d60377fbbf439cd831
cmhoove14 <cmhoove14@gmail.com>
2015-12-10 11:29:17 -0500

    Base model that approximately replicates Sokolow et al 2015 and is used to replicate their fig4 results

--- schisto_base.R
+++ schisto_base.R
@@ -1,4 +1,11 @@
 ## A schisto model
+#This work is licensed under a Creative Commons Attribution-NonCommercial 4.0 International License#########
+#<http://creativecommons.org/licenses/by-nc/4.0/> by Christopher Hoover, Arathi Arakala, Manoj Gambhir 
+#and Justin Remais. This work was supported in part by the National Institutes of Health/National Science 
+#Foundation Ecology of Infectious Disease program funded by the Fogarty International Center 
+#(grant R01TW010286), the National Institute of Allergy and Infectious Diseases (grant K01AI091864), 
+#and the National Science Foundation Water Sustainability and Climate Program (grant 1360330).
+
 library(deSolve)
 schisto=function(t, n, parameters) { 
   with(as.list(parameters),{
