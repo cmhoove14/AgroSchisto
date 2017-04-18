8c553d7fdb55dfee4dfcbb89beee92525866f4b8
cmhoove14 <cmhoove14@gmail.com>
2017-04-18 15:57:29 -0700

    Updated folder structure, post-processing code for savio output from stochastic modeling, etc.

--- Elimination_Feasibility/stochModSims_postProcess.R
+++ Elimination_Feasibility/stochModSims_postProcess.R
@@ -10,13 +10,16 @@
 #and indicate changes that were made.###############
 
 #Load datasets from simulations with no observation noise #########
+library(ggplot2)
 par.mat = read.csv('Elimination_Feasibility/eq_vals_for_trans_pars.csv')   #load parameter sets data frame with eq
   par.mat$eps = NA       #add column for elim. feas estimator (eps)
   par.mat$eps.sd = NA    #add column for elim. feas estimator st. dev 
   par.mat$pe = NA        #add column for proba elimination (P(e))
+  
+mat.all = data.frame()
 
-#Use array loads from sim runs on savio to fill the data frame
-load('Elimination_Feasibility/Savio/fill_array1_500.Rdata')
+#Use array loads from sim runs on savio to fill the data frame #########
+load('Elimination_Feasibility/Savio/Results/fill_array1_500.Rdata')
   inarr = fill.arr
   chunk = 1
   lo = chunk*dim(inarr)[1] - (dim(inarr)[1]-1)
@@ -29,7 +32,7 @@ load('Elimination_Feasibility/Savio/fill_array1_500.Rdata')
     #print(m)
   }
   
-load('Elimination_Feasibility/Savio/fill_array501_1000.Rdata')
+load('Elimination_Feasibility/Savio/Results/fill_array501_1000.Rdata')
   inarr = fill.arr
   chunk = 2
   lo = chunk*dim(inarr)[1] - (dim(inarr)[1]-1)
@@ -41,7 +44,7 @@ load('Elimination_Feasibility/Savio/fill_array501_1000.Rdata')
     par.mat$pe[m+lo-1] = sum(inarr[m, 3, ]) / dim(inarr)[3]  #P(e) = sims that end in elimination / total sims
     #print(m)
   }
-load('Elimination_Feasibility/Savio/fill_array1001_1500.Rdata')
+load('Elimination_Feasibility/Savio/Results/fill_array1001_1500.Rdata')
   inarr = fill.arr
   chunk = 3
   lo = chunk*dim(inarr)[1] - (dim(inarr)[1]-1)
@@ -53,7 +56,7 @@ load('Elimination_Feasibility/Savio/fill_array1001_1500.Rdata')
     par.mat$pe[m+lo-1] = sum(inarr[m, 3, ]) / dim(inarr)[3]  #P(e) = sims that end in elimination / total sims
     #print(m)
   }
-load('Elimination_Feasibility/Savio/fill_array1501_2000.Rdata')
+load('Elimination_Feasibility/Savio/Results/fill_array1501_2000.Rdata')
   inarr = fill.arr
   chunk = 4
   lo = chunk*dim(inarr)[1] - (dim(inarr)[1]-1)
@@ -65,7 +68,7 @@ load('Elimination_Feasibility/Savio/fill_array1501_2000.Rdata')
     par.mat$pe[m+lo-1] = sum(inarr[m, 3, ]) / dim(inarr)[3]  #P(e) = sims that end in elimination / total sims
     #print(m)
   }
-load('Elimination_Feasibility/Savio/fill_array2001_2500.Rdata')
+load('Elimination_Feasibility/Savio/Results/fill_array2001_2500.Rdata')
   inarr = fill.arr
   chunk = 5
   lo = chunk*dim(inarr)[1] - (dim(inarr)[1]-1)
@@ -78,32 +81,68 @@ load('Elimination_Feasibility/Savio/fill_array2001_2500.Rdata')
     #print(m)
   }
 
-par.mat2 = par.mat  
+par.mat2 = par.mat 
+  colnames(par.mat2)[c(1:7)] = c('lambda', 'kappa', 'S', 'E', 'I', 'Wt', 'Wu')
+rm(list = c('par.mat'))  
+#plots of and analysis of pe(e) / eps with no obs noise #######
 #plot P(e) across eps
 plot(par.mat2$eps, par.mat2$pe, pch = 18, cex = 0.6, ylim = c(0,1),
      xlab = expression(epsilon), ylab = expression(italic('P(e)')))
-  #for(e in 1:nrow(par.mat2)){
-  #    segments(y0 = par.mat2$pe[e], y1 = par.mat2$pe[e],
-  #             x0 = par.mat2$eps[e] - par.mat2$eps.sd[e],
-  #             x1 = par.mat2$eps[e] + par.mat2$eps.sd[e])
-  #  } #plot error bars, but makes the plot unreadable
+
+#ggplot to show val of lambda / kappa in addition to scatterP
+ggplot(data = par.mat2, aes(x = eps, y = pe, col = lambda)) +   #V2 for kappa, V1 for lambda
+  theme_bw() +
+  geom_point(shape = 18, size = 1.2) +
+  scale_color_gradient(low = 'wheat1', high = 'red') +
+  labs(col = expression(lambda),
+       x = expression(epsilon),
+       y = expression(italic(P(e))))
 
 #only plot for vals where p(e) != 1 or 0
-par.mat0_1 = subset(par.mat2, pe != 0 & pe != 1 & V2 != 0)
+par.mat0_1 = subset(par.mat2, pe != 0 & pe != 1 & kappa != 0)
 
 plot(par.mat0_1$eps, par.mat0_1$pe, pch = 18, cex = 0.6, ylim = c(0,1),
-     xlab = expression(epsilon), ylab = expression(italic('P(e)')))
+     xlab = expression(epsilon), ylab = '',
+     cex.lab = 1.4)
+  mtext(side = 2, text = expression(italic('P(e)')), line = 2.4, cex = 1.2)
+
 
 #logit transform p(e)
 plot(par.mat0_1$eps, log(par.mat0_1$pe / (1 - par.mat0_1$pe)), 
      pch = 18, cex = 0.6, #ylim = c(0,1),
      xlab = expression(epsilon), ylab = expression(italic('P(e)')))
 
-#regression
-pe.mod = lm(log(par.mat0_1$pe / (1 - par.mat0_1$pe)) ~ par.mat0_1$eps, data = par.mat0_1)
+#regression of eps to pe ########
+#with manual logit transform
+pe.mod = lm(log(pe / (1 - pe)) ~ eps, data = par.mat0_1)
   summary(pe.mod)
   abline(a = pe.mod$coefficients[1], b = pe.mod$coefficients[2],
-         lty = 2, col = 2)
+         lty = 2, col = 2, lwd = 2)
+#weighted
+pe.mod.weighted = lm(log(pe / (1 - pe)) ~ eps, data = par.mat0_1, weights = eps.sd)
+  summary(pe.mod.weighted)
+  #abline(a = pe.mod.weighted$coefficients[1], b = pe.mod.weighted$coefficients[2],
+  #       lty = 2, col = 3, lwd = 2)
+  
+#Doesn't look like adding weights changes much, so stick with unweighted regression  
+  
+#including transmission intensity and pdd as regression coefficients
+pe.mod.all =  lm(log(pe / (1 - pe)) ~ eps + kappa + lambda, data = par.mat0_1) 
+  summary(pe.mod.all)
+AIC(pe.mod, pe.mod.all)  #looks like this actually produces a better model... 
+  
+#plot regression prediction with actual data #########
+  plot(par.mat0_1$eps, par.mat0_1$pe, pch = 18, cex = 0.6, ylim = c(0,1),
+       xlab = expression(epsilon), ylab = expression(italic('P(e)')))
+  
+  back.trans = function(eps){
+    p = exp(pe.mod$coefficients[1] + eps*pe.mod$coefficients[2])
+    pe.pred = p / (1+p)
+    return(pe.pred)
+  }
+    lines(seq(-0.05, 0.02, by = 0.001), sapply(seq(-0.05, 0.02, by = 0.001), back.trans, simplify = T),
+          lty = 2,col = 2, lwd = 2)
+#other plots #######    
 #plot relationship between eps and its standard deviation
 plot(par.mat2$eps, par.mat2$eps.sd, pch = 18, cex = 0.6, #ylim = c(0,1),
      ylab = expression(paste(epsilon, ' St. Dev.', sep='')), 
@@ -121,87 +160,73 @@ plot(par.mat2$V2, par.mat2$pe, pch = 18, cex = 0.6, ylim = c(0,1),
 lams = unique(par.mat2$V1)
 kaps = unique(par.mat2$V2)
 
-lkmat = matrix(par.mat2$pe, ncol=50, nrow=50)
+lkmat = matrix(par.mat2$pe, ncol=50, nrow=50, byrow = T)
 
-persp(x = lams, xlim = range(lams), y = kaps, ylim = range(kaps),
+persp(y = lams, ylim = range(lams), x = kaps, xlim = range(kaps),
       z = lkmat, ticktype = 'detailed', nticks = 4, 
-      ylab = 'Pos. Density Dependence',
-      xlab = 'Transmission Intensity',
+      xlab = 'Pos. Density Dependence',
+      ylab = 'Transmission Intensity',
       zlab = 'Probability of Elimination',
-      phi = 20, theta = 85, shade = 0.4, col = 'lightblue')
+      phi = 20, theta = 225, shade = 0.4, col = 'lightblue')
 
-#Same routine for simulations with observation noise #########
-par.mat = read.csv('Elimination_Feasibility/eq_vals_for_trans_pars.csv')   #load parameter sets data frame with eq
-  par.mat$eps = NA       #add column for elim. feas estimator (eps)
-  par.mat$eps.sd = NA    #add column for elim. feas estimator st. dev 
-  par.mat$pe = NA        #add column for proba elimination (P(e))
 
-load('Elimination_Feasibility/Savio/wNoise_array1_500.Rdata')
+#Use array loads from sim runs on savio to create new data frame with each row as a simulation #########
+mat.all = data.frame()
+
+load('Elimination_Feasibility/Savio/Results/fill_array1_500.Rdata')
   inarr = fill.arr
   chunk = 1
   lo = chunk*dim(inarr)[1] - (dim(inarr)[1]-1)
   hi = chunk*dim(inarr)[1]
   
-  for(m in 1:500){
-    par.mat$eps[m+lo-1] = mean(inarr[m, 4, ])
-    par.mat$eps.sd[m+lo-1] = sd(inarr[m, 4, ])
-    par.mat$pe[m+lo-1] = sum(inarr[m, 3, ]) / dim(inarr)[3]  #P(e) = sims that end in elimination / total sims
-    #print(m)
+  for(j in 1:500){
+    mat.all = rbind(mat.all, t(inarr[j, , ]))
   }
-
-load('Elimination_Feasibility/Savio/wNoise_array501_1000.Rdata')
-inarr = fill.arr
-chunk = 2
+  
+load('Elimination_Feasibility/Savio/Results/fill_array501_1000.Rdata')
+  inarr = fill.arr
+  chunk = 2
   lo = chunk*dim(inarr)[1] - (dim(inarr)[1]-1)
   hi = chunk*dim(inarr)[1]
   
-  for(m in 1:500){
-    par.mat$eps[m+lo-1] = mean(inarr[m, 4, ])
-    par.mat$eps.sd[m+lo-1] = sd(inarr[m, 4, ])
-    par.mat$pe[m+lo-1] = sum(inarr[m, 3, ]) / dim(inarr)[3]  #P(e) = sims that end in elimination / total sims
-    #print(m)
+  for(j in 1:500){
+    mat.all = rbind(mat.all, t(inarr[j, , ]))
   }
-
-load('Elimination_Feasibility/Savio/wNoise_array1001_1500.Rdata')
-inarr = fill.arr
-chunk = 3
+  
+load('Elimination_Feasibility/Savio/Results/fill_array1001_1500.Rdata')
+  inarr = fill.arr
+  chunk = 3
   lo = chunk*dim(inarr)[1] - (dim(inarr)[1]-1)
   hi = chunk*dim(inarr)[1]
   
-  for(m in 1:500){
-    par.mat$eps[m+lo-1] = mean(inarr[m, 4, ])
-    par.mat$eps.sd[m+lo-1] = sd(inarr[m, 4, ])
-    par.mat$pe[m+lo-1] = sum(inarr[m, 3, ]) / dim(inarr)[3]  #P(e) = sims that end in elimination / total sims
-    #print(m)
+  for(j in 1:500){
+    mat.all = rbind(mat.all, t(inarr[j, , ]))
   }
-
-load('Elimination_Feasibility/Savio/wNoise_array1501_2000.Rdata')
-inarr = fill.arr
-chunk = 4
+  
+load('Elimination_Feasibility/Savio/Results/fill_array1501_2000.Rdata')
+  inarr = fill.arr
+  chunk = 4
   lo = chunk*dim(inarr)[1] - (dim(inarr)[1]-1)
   hi = chunk*dim(inarr)[1]
   
-  for(m in 1:500){
-    par.mat$eps[m+lo-1] = mean(inarr[m, 4, ])
-    par.mat$eps.sd[m+lo-1] = sd(inarr[m, 4, ])
-    par.mat$pe[m+lo-1] = sum(inarr[m, 3, ]) / dim(inarr)[3]  #P(e) = sims that end in elimination / total sims
-    #print(m)
+  for(j in 1:500){
+    mat.all = rbind(mat.all, t(inarr[j, , ]))
   }
-
-load('Elimination_Feasibility/Savio/wNoise_array2001_2500.Rdata')
-inarr = fill.arr
-chunk = 5
+  
+load('Elimination_Feasibility/Savio/Results/fill_array2001_2500.Rdata')
+  inarr = fill.arr
+  chunk = 5
   lo = chunk*dim(inarr)[1] - (dim(inarr)[1]-1)
   hi = chunk*dim(inarr)[1]
   
-  for(m in 1:500){
-    par.mat$eps[m+lo-1] = mean(inarr[m, 4, ])
-    par.mat$eps.sd[m+lo-1] = sd(inarr[m, 4, ])
-    par.mat$pe[m+lo-1] = sum(inarr[m, 3, ]) / dim(inarr)[3]  #P(e) = sims that end in elimination / total sims
-    #print(m)
+  for(j in 1:500){
+    mat.all = rbind(mat.all, t(inarr[j, , ]))
   }
-
-#plot P(e) across eps
-plot(par.mat$eps, par.mat$pe, pch = 18, cex = 0.6, ylim = c(0,1),
-     xlab = expression(epsilon), ylab = expression(italic('P(e)')))
   
+colnames(mat.all) = c('kappa', 'lambda', 'pe', 'eps')
+
+#logistic regression with glm 
+pe.glm = glm(pe ~ eps + kappa + lambda, data = mat.all, 
+             family = 'binomial'(link = 'logit'))
+summary(pe.glm)
+
---
+++ Elimination_Feasibility/stochModSims_postProcess_wNoise.R
@@ -0,0 +1,207 @@
+#This work is licensed under a Creative Commons Attribution-NonCommercial 4.0 International License#########
+#<http://creativecommons.org/licenses/by-nc/4.0/> by Christopher Hoover, Arathi Arakala, Manoj Gambhir 
+#and Justin Remais. This work was supported in part by the National Institutes of Health/National Science 
+#Foundation Ecology of Infectious Disease program funded by the Fogarty International Center 
+#(grant R01TW010286), the National Institute of Allergy and Infectious Diseases (grant K01AI091864), 
+#and the National Science Foundation Water Sustainability and Climate Program (grant 1360330).
+
+#Per the terms of this license, if you are making derivative use of this work, you must identify that 
+#your work is a derivative work, give credit to the original work, provide a link to the license, 
+#and indicate changes that were made.###############
+
+#Load datasets from simulations with observation noise #########
+library(ggplot2)
+library(aod)
+par.mat = read.csv('Elimination_Feasibility/eq_vals_for_trans_pars.csv')   #load parameter sets data frame with eq
+  par.mat$eps = NA       #add column for elim. feas estimator (eps)
+  par.mat$eps.sd = NA    #add column for elim. feas estimator st. dev 
+  par.mat$pe = NA        #add column for proba elimination (P(e))
+  
+#Same routine for simulations with observation noise #########
+load('Elimination_Feasibility/Savio/Results/wNoise_array1_500.Rdata')
+  inarr = fill.arr
+  chunk = 1
+  lo = chunk*dim(inarr)[1] - (dim(inarr)[1]-1)
+  hi = chunk*dim(inarr)[1]
+  
+  for(m in 1:500){
+    par.mat$eps[m+lo-1] = mean(inarr[m, 4, ])
+    par.mat$eps.sd[m+lo-1] = sd(inarr[m, 4, ])
+    par.mat$pe[m+lo-1] = sum(inarr[m, 3, ]) / dim(inarr)[3]  #P(e) = sims that end in elimination / total sims
+    #print(m)
+  }
+
+load('Elimination_Feasibility/Savio/Results/wNoise_array501_1000.Rdata')
+inarr = fill.arr
+chunk = 2
+  lo = chunk*dim(inarr)[1] - (dim(inarr)[1]-1)
+  hi = chunk*dim(inarr)[1]
+  
+  for(m in 1:500){
+    par.mat$eps[m+lo-1] = mean(inarr[m, 4, ])
+    par.mat$eps.sd[m+lo-1] = sd(inarr[m, 4, ])
+    par.mat$pe[m+lo-1] = sum(inarr[m, 3, ]) / dim(inarr)[3]  #P(e) = sims that end in elimination / total sims
+    #print(m)
+  }
+
+load('Elimination_Feasibility/Savio/Results/wNoise_array1001_1500.Rdata')
+inarr = fill.arr
+chunk = 3
+  lo = chunk*dim(inarr)[1] - (dim(inarr)[1]-1)
+  hi = chunk*dim(inarr)[1]
+  
+  for(m in 1:500){
+    par.mat$eps[m+lo-1] = mean(inarr[m, 4, ])
+    par.mat$eps.sd[m+lo-1] = sd(inarr[m, 4, ])
+    par.mat$pe[m+lo-1] = sum(inarr[m, 3, ]) / dim(inarr)[3]  #P(e) = sims that end in elimination / total sims
+    #print(m)
+  }
+
+load('Elimination_Feasibility/Savio/Results/wNoise_array1501_2000.Rdata')
+inarr = fill.arr
+chunk = 4
+  lo = chunk*dim(inarr)[1] - (dim(inarr)[1]-1)
+  hi = chunk*dim(inarr)[1]
+  
+  for(m in 1:500){
+    par.mat$eps[m+lo-1] = mean(inarr[m, 4, ])
+    par.mat$eps.sd[m+lo-1] = sd(inarr[m, 4, ])
+    par.mat$pe[m+lo-1] = sum(inarr[m, 3, ]) / dim(inarr)[3]  #P(e) = sims that end in elimination / total sims
+    #print(m)
+  }
+
+load('Elimination_Feasibility/Savio/Results/wNoise_array2001_2500.Rdata')
+inarr = fill.arr
+chunk = 5
+  lo = chunk*dim(inarr)[1] - (dim(inarr)[1]-1)
+  hi = chunk*dim(inarr)[1]
+  
+  for(m in 1:500){
+    par.mat$eps[m+lo-1] = mean(inarr[m, 4, ])
+    par.mat$eps.sd[m+lo-1] = sd(inarr[m, 4, ])
+    par.mat$pe[m+lo-1] = sum(inarr[m, 3, ]) / dim(inarr)[3]  #P(e) = sims that end in elimination / total sims
+    #print(m)
+  }
+
+colnames(par.mat)[c(1:7)] = c('lambda', 'kappa', 'S', 'E', 'I', 'Wt', 'Wu')  
+
+#Some summaries of P(e) across the transmission parameters ##########
+mean(par.mat$pe[par.mat$lambda == min(par.mat$lambda)])
+mean(par.mat$pe[par.mat$lambda == max(par.mat$lambda)])
+
+mean(par.mat$pe[par.mat$kappa == min(par.mat$kappa)])
+mean(par.mat$pe[par.mat$kappa == max(par.mat$kappa)])
+
+mean(par.mat$pe[round(par.mat$eps, digits = 3) == 0])
+
+lams = unique(par.mat$lambda)
+kaps = unique(par.mat$kappa)
+
+lkmat.pe = matrix(par.mat$pe, ncol=50, nrow=50, byrow = T)
+            colnames(lkmat.pe) = round(unique(lams), digits = 6)
+            rownames(lkmat.pe) = round(unique(kaps), digits = 3)
+  
+  heat.pe = data.matrix(lkmat.pe)
+  
+lkmat.eps = matrix(par.mat$eps, ncol=50, nrow=50, byrow = T)
+  colnames(lkmat.eps) = round(unique(lams), digits = 6)
+  rownames(lkmat.eps) = round(unique(kaps), digits = 3)
+  
+  heat.eps = data.matrix(lkmat.eps)
+  
+heatmap(heat.pe, Rowv=NA, Colv=NA, col = heat.colors(256), scale="column")
+heatmap(heat.eps, Rowv=NA, Colv=NA, col = heat.colors(256), scale="column")
+
+
+#plots of and analysis of pe(e) / eps with obs noise #######
+plot(par.mat$eps, par.mat$pe, pch = 18, cex = 0.6, ylim = c(0,1),
+     xlab = expression(epsilon), ylab = expression(italic('P(e)')))
+  
+#ggplot to show val of lambda / kappa in addition to scatterP
+  ggplot(data = par.mat, aes(x = eps, y = pe, col = lambda)) +   #V2 for kappa, V1 for lambda
+    theme_bw() +
+    geom_point(shape = 18, size = 1.2) +
+    scale_color_gradient(low = 'wheat1', high = 'red') +
+    labs(col = expression(lambda),
+         x = expression(epsilon),
+         y = expression(italic(P(e))))
+  
+#only plot for vals where p(e) != 1 or 0
+  par.mat01 = subset(par.mat, pe != 0 & pe != 1 & kappa != 0)
+  
+  plot(par.mat01$eps, par.mat01$pe, pch = 18, cex = 0.6, ylim = c(0,1),
+       xlab = expression(epsilon), ylab = '',
+       cex.lab = 1.4)
+    mtext(side = 2, text = expression(italic('P(e)')), line = 2.4, cex = 1.2)
+  
+  
+#logit transform p(e)
+  plot(par.mat01$eps, log(par.mat01$pe / (1 - par.mat01$pe)), 
+       pch = 18, cex = 0.6, #ylim = c(0,1),
+       xlab = expression(epsilon), ylab = expression(italic('P(e)')))
+  
+#regression of eps to pe ########
+  #with manual logit transform
+  pe.mod = lm(log(pe / (1 - pe)) ~ eps, data = par.mat01)
+    summary(pe.mod)
+  abline(a = pe.mod$coefficients[1], b = pe.mod$coefficients[2],
+         lty = 2, col = 2, lwd = 2)
+  
+  #weighted based on inverse of st. dev of epsilon
+  pe.mod.weight = lm(log(pe / (1 - pe)) ~ eps, data = par.mat01, weights = eps.sd^-1)
+    summary(pe.mod.weight)
+    
+  #doesn't change results  
+    
+#regression of eps to lambda and kappa ########
+  plot(par.mat01$lambda, log(par.mat01$pe / (1 - par.mat01$pe)), 
+       pch = 18, cex = 0.6, #ylim = c(0,1),
+       xlab = expression(lambda), ylab = expression(italic('P(e)')))
+  pe.mod.lam = lm(log(pe / (1 - pe)) ~ lambda, data = par.mat01)
+    summary(pe.mod.lam)
+    abline(a = pe.mod.lam$coefficients[1], b = pe.mod.lam$coefficients[2],
+           lty = 2, col = 2, lwd = 2)
+    
+  plot(par.mat01$kappa, log(par.mat01$pe / (1 - par.mat01$pe)), 
+       pch = 18, cex = 0.6, #ylim = c(0,1),
+       xlab = expression(kappa), ylab = expression(italic('P(e)')))
+  pe.mod.kap = lm(log(pe / (1 - pe)) ~ kappa, data = par.mat01)
+    summary(pe.mod.kap)
+    abline(a = pe.mod.kap$coefficients[1], b = pe.mod.kap$coefficients[2],
+           lty = 2, col = 2, lwd = 2)
+    
+
+#plot residuals of logit(p(e)) ~ lambda across epsilon
+  plot(par.mat01$eps, resid(pe.mod.lam), pch = 18, cex = 0.6) 
+    lm.res.eps = lm(resid(pe.mod.lam) ~ par.mat01$eps)
+      summary(lm.res.eps)
+    abline(a = lm.res.eps$coefficients[1], b = lm.res.eps$coefficients[2],
+           lty = 2, col = 2, lwd = 2)
+    
+#plot residuals of logit(p(e)) ~ kappa across epsilon and lambda
+  plot(par.mat01$eps, resid(pe.mod.kap), pch = 18, cex = 0.6) 
+    lm.res.kap = lm(resid(pe.mod.kap) ~ par.mat01$eps)
+    summary(lm.res.kap)
+    abline(a = lm.res.kap$coefficients[1], b = lm.res.kap$coefficients[2],
+           lty = 2, col = 2, lwd = 2)
+    
+  plot(par.mat01$lambda, resid(pe.mod.kap), pch = 18, cex = 0.6) 
+    lm.res.lam = lm(resid(pe.mod.kap) ~ par.mat01$lambda)
+    summary(lm.res.lam)
+    abline(a = lm.res.lam$coefficients[1], b = lm.res.lam$coefficients[2],
+           lty = 2, col = 2, lwd = 2)
+    
+#definitely cross-correlation between lambda and epsilon... check with plot
+  plot(par.mat01$lambda, par.mat01$eps, 
+       pch = 18, cex = 0.6, #ylim = c(0,1),
+       ylab = expression(epsilon), xlab = expression(lambda))
+  eps.lam.mod = lm(eps ~ lambda, data = par.mat01)
+      summary(eps.lam.mod)
+    abline(a = eps.lam.mod$coefficients[1], b = eps.lam.mod$coefficients[2],
+           lty = 2, col = 2, lwd = 2)
+    
+#Formal nested regression modeling ##########
+  mod.lek = lm(log(pe / (1 - pe)) ~ eps + lambda * kappa, data = par.mat01)
+  
+  anova(mod.lek)
+  
