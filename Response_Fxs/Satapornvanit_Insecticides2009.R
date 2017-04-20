2faaf202bd073f849f859651f14d5046c66dc96a
cmhoove14 <cmhoove14@gmail.com>
2017-04-20 14:08:09 -0700

    updates to satapornvanit 2009 toxicity and feeding reductions

--- Response_Fxs/Satapornvanit_Insecticides2009.R
+++ Response_Fxs/Satapornvanit_Insecticides2009.R
@@ -12,84 +12,81 @@ require(drc)
     
 #Zinc ########    
   muPq_zinc_satapornvanit09<-function(In){
-    predict(sap.mupq, data.frame(conc=In, chem = 'zinc'))
+    predict(sap.mupq, data.frame(conc=In, chem = 'zinc'), interval = 'confidence', level = 0.95)
   }  
     
-    zinc.sat09.df = data.frame(conc = c(0:1000),
-                               chem = 'zinc',
-                               Prediction = 0,
-                               Lower = 0,
-                               Upper = 0)
-    
-    zinc.sat09.df[,3:5] <- predict(sap.mupq, newdata = zinc.sat09.df, 
-                                   interval = 'confidence', level = 0.95)
-    
   plot(sap.mort$conc[sap.mort$chem == 'zinc'], sap.mort$mort[sap.mort$chem == 'zinc']/100, ylim = c(0,1),
-       pch = 16, xlab = 'Zinc concentration (ppb)', ylab = 'prop dead', main = 'Zinc daily toxicity to M. rosenbergii')
-    lines(zinc.sat09.df$conc, zinc.sat09.df$Prediction, lty=2, col=2)
-    lines(zinc.sat09.df$conc, zinc.sat09.df$Lower, lty=3, col=2)
-    lines(zinc.sat09.df$conc, zinc.sat09.df$Upper, lty=3, col=2)
+       pch = 16, xlab = 'Zinc concentration (ppb)', ylab = 'prop dead', 
+       main = expression(paste('Zinc daily toxicity to post-larval ', italic('M. rosenbergii'), sep = '')))
+    lines(seq(0,900,10), sapply(seq(0,900,10), muPq_zinc_satapornvanit09, simplify = T)[1,], lty=2, col=2)
+    lines(seq(0,900,10), sapply(seq(0,900,10), muPq_zinc_satapornvanit09, simplify = T)[2,], lty=3, col=2)
+    lines(seq(0,900,10), sapply(seq(0,900,10), muPq_zinc_satapornvanit09, simplify = T)[3,], lty=3, col=2)
+    
+  muPq_zinc_satapornvanit09_uncertainty<-function(In){
+    rdrm(1, LL.2(), coef(sap.mupq)[c(1,5)], In, yerror = 'rbinom', ypar = 30)$y / 30
+  }
+    
+  points(seq(0,900,10), sapply(seq(0,900,10), muPq_zinc_satapornvanit09_uncertainty, simplify = T), 
+         pch=5, col=4, cex = 0.5)
+  
     
 #Chlorpyrifos ###########    
   muPq_chlor_satapornvanit09<-function(In){
-    predict(sap.mupq, data.frame(conc=In, chem = 'chlorpyrifos'))
+    predict(sap.mupq, data.frame(conc=In, chem = 'chlorpyrifos'), interval = 'confidence', level = 0.95)
   }  
-  
-      chlor.sat09.df = data.frame(conc = seq(0,10,0.01),
-                                 chem = 'chlorpyrifos',
-                                 Prediction = 0,
-                                 Lower = 0,
-                                 Upper = 0)
-    
-    chlor.sat09.df[,3:5] <- predict(sap.mupq, newdata = chlor.sat09.df, 
-                                   interval = 'confidence', level = 0.95)
     
     plot(sap.mort$conc[sap.mort$chem == 'chlorpyrifos'], sap.mort$mort[sap.mort$chem == 'chlorpyrifos']/100, ylim = c(0,1),
-         pch = 16, xlab = 'chlorpyrifos concentration (ppb)', ylab = 'prop dead', main = 'chlorpyrifos daily toxicity to M. rosenbergii')
-      lines(chlor.sat09.df$conc, chlor.sat09.df$Prediction, lty=2, col=2)
-      lines(chlor.sat09.df$conc, chlor.sat09.df$Lower, lty=3, col=2)
-      lines(chlor.sat09.df$conc, chlor.sat09.df$Upper, lty=3, col=2)
-
+         pch = 16, xlab = 'chlorpyrifos concentration (ppb)', ylab = 'prop dead', 
+         main = expression(paste('Chlorpyrifos daily toxicity to post-larval ', italic('M. rosenbergii'), sep = '')))
+      lines(seq(0,5,0.01), sapply(seq(0,5,0.01), muPq_chlor_satapornvanit09, simplify = T)[1,], lty=2, col=2)
+      lines(seq(0,5,0.01), sapply(seq(0,5,0.01), muPq_chlor_satapornvanit09, simplify = T)[2,], lty=3, col=2)
+      lines(seq(0,5,0.01), sapply(seq(0,5,0.01), muPq_chlor_satapornvanit09, simplify = T)[3,], lty=3, col=2)
+    
+  muPq_chlor_satapornvanit09_uncertainty<-function(In){
+    rdrm(1, LL.2(), coef(sap.mupq)[c(2,6)], In, yerror = 'rbinom', ypar = 30)$y / 30
+  }
+      
+  points(seq(0,5,0.1), sapply(seq(0,5,0.1), muPq_chlor_satapornvanit09_uncertainty, simplify = T), 
+          pch=5, col=4, cex = 0.5)    
+      
 #Dimethoate ################        
   muPq_dim_satapornvanit09<-function(In){
-    predict(sap.mupq, data.frame(conc=In, chem = 'dimethoate'))
+    predict(sap.mupq, data.frame(conc=In, chem = 'dimethoate'), interval = 'confidence', level = 0.95)
   }  
+  
+  plot(sap.mort$conc[sap.mort$chem == 'dimethoate'], sap.mort$mort[sap.mort$chem == 'dimethoate']/100, ylim = c(0,1),
+       pch = 16, xlab = 'dimethoate concentration (ppb)', ylab = 'prop dead', 
+       main = expression(paste('Dimethoate daily toxicity to post-larval ', italic('M. rosenbergii'), sep = '')))
+    lines(seq(0,1300,10), sapply(seq(0,1300,10), muPq_dim_satapornvanit09, simplify = T)[1,], lty=2, col=2)
+    lines(seq(0,1300,10), sapply(seq(0,1300,10), muPq_dim_satapornvanit09, simplify = T)[2,], lty=3, col=2)
+    lines(seq(0,1300,10), sapply(seq(0,1300,10), muPq_dim_satapornvanit09, simplify = T)[3,], lty=3, col=2)
       
-    dim.sat09.df = data.frame(conc = c(0:1300),
-                                  chem = 'dimethoate',
-                                  Prediction = 0,
-                                  Lower = 0,
-                                  Upper = 0)
-      
-      dim.sat09.df[,3:5] <- predict(sap.mupq, newdata = dim.sat09.df, 
-                                      interval = 'confidence', level = 0.95)
-      
-    plot(sap.mort$conc[sap.mort$chem == 'dimethoate'], sap.mort$mort[sap.mort$chem == 'dimethoate']/100, ylim = c(0,1),
-         pch = 16, xlab = 'dimethoate concentration (ppb)', ylab = 'prop dead', main = 'dimethoate daily toxicity to M. rosenbergii')
-      lines(dim.sat09.df$conc, dim.sat09.df$Prediction, lty=2, col=2)
-      lines(dim.sat09.df$conc, dim.sat09.df$Lower, lty=3, col=2)
-      lines(dim.sat09.df$conc, dim.sat09.df$Upper, lty=3, col=2)
+  muPq_dim_satapornvanit09_uncertainty<-function(In){
+    rdrm(1, LL.2(), coef(sap.mupq)[c(3,7)], In, yerror = 'rbinom', ypar = 30)$y / 30
+  }
+    
+  points(seq(0,1300,10), sapply(seq(0,1300,10), muPq_dim_satapornvanit09_uncertainty, simplify = T), 
+          pch=5, col=4, cex = 0.5)       
 
 #Profenofos ################      
   muPq_pr_satapornvanit09<-function(In){
-    predict(sap.mupq, data.frame(conc=In, chem = 'profenofos'))
+    predict(sap.mupq, data.frame(conc=In, chem = 'profenofos'), interval = 'confidence', level = 0.95)
   }  
       
-    prof.sat09.df = data.frame(conc = seq(0,60,0.1),
-                                chem = 'profenofos',
-                                Prediction = 0,
-                                Lower = 0,
-                                Upper = 0)
-      
-      prof.sat09.df[,3:5] <- predict(sap.mupq, newdata = prof.sat09.df, 
-                                    interval = 'confidence', level = 0.95)
-      
-    plot(sap.mort$conc[sap.mort$chem == 'profenofos'], sap.mort$mort[sap.mort$chem == 'profenofos']/100, ylim = c(0,1),
-         pch = 16, xlab = 'profenofos concentration (ppb)', ylab = 'prop dead', main = 'profenofos daily toxicity to M. rosenbergii')
-      lines(prof.sat09.df$conc, prof.sat09.df$Prediction, lty=2, col=2)
-      lines(prof.sat09.df$conc, prof.sat09.df$Lower, lty=3, col=2)
-      lines(prof.sat09.df$conc, prof.sat09.df$Upper, lty=3, col=2)
-  
+  plot(sap.mort$conc[sap.mort$chem == 'profenofos'], sap.mort$mort[sap.mort$chem == 'profenofos']/100, ylim = c(0,1),
+       pch = 16, xlab = 'profenofos concentration (ppb)', ylab = 'prop dead', 
+       main = expression(paste('Profenofos daily toxicity to post-larval ', italic('M. rosenbergii'), sep = '')))      
+    lines(seq(0,60,0.1), sapply(seq(0,60,0.1), muPq_pr_satapornvanit09, simplify = T)[1,], lty=2, col=2)
+    lines(seq(0,60,0.1), sapply(seq(0,60,0.1), muPq_pr_satapornvanit09, simplify = T)[2,], lty=3, col=2)
+    lines(seq(0,60,0.1), sapply(seq(0,60,0.1), muPq_pr_satapornvanit09, simplify = T)[3,], lty=3, col=2)
+ 
+  muPq_prof_satapornvanit09_uncertainty<-function(In){
+    rdrm(1, LL.2(), coef(sap.mupq)[c(4,8)], In, yerror = 'rbinom', ypar = 30)$y / 30
+  }
+    
+  points(seq(0,60,0.5), sapply(seq(0,60,0.5), muPq_prof_satapornvanit09_uncertainty, simplify = T), 
+          pch=5, col=4, cex = 0.5)   
+#Carbendazim ###########       
   muPq_carb_satapornvanit09<-function(In){ #Paper found no consistent effect of carbendazim on mortality, even
       0*In                                 #at levels higher than the solubility of carbendazim in water
   }  
@@ -105,53 +102,59 @@ sap.fr<-read.csv("C:/Users/chris_hoover/Documents/RemaisWork/Schisto/Data/AgroDa
                fct = LL.4(names = c("Slope", "Lower Limit", "Upper Limit", "ED50"),
                           fixed = c(NA, 0, max(fr.z$feed_rate), NA)))
     summary(zinc.fr)
-    plot(zinc.fr)
     
   psi_q_zinc_satapornvanit09<-function(In){
-    predict(zinc.fr, data.frame(conc = In)) / predict(zinc.fr, data.frame(conc = 0))
+    predict(zinc.fr, data.frame(conc = In), interval = 'confidence', level = 0.95) / fr.z$feed_rate[1]
   }  
     
   plot(fr.z$conc, fr.z$feed_rate / fr.z$feed_rate[1], pch = 16, ylim = c(0,1),
-       xlab = 'Zinc concentration (ppb)', ylab = 'Feeding rate (prey/prawn/hr)', main = 'Prawn reduced feeding (Sata-09)')
-  
-  fr.z.df = data.frame(conc = c(0:1100),
-                       Prediction = 0,
-                       Lower = 0,
-                       Upper = 0)
-  
-  fr.z.df[,2:4] <- predict(zinc.fr, newdata = fr.z.df, 
-                           interval = 'confidence', level = 0.95)
-  
-  
-  lines(fr.z.df$conc, fr.z.df$Prediction / fr.z.df$Prediction[1], col = 2, lty=2)
-  lines(fr.z.df$conc, fr.z.df$Lower / fr.z.df$Prediction[1], col = 2, lty=3)
-  lines(fr.z.df$conc, fr.z.df$Upper / fr.z.df$Prediction[1], col = 2, lty=3)  
-  
+       xlab = 'Zinc concentration (ppb)', ylab = 'Feeding rate (prey/prawn/hr)', 
+       main = 'Prawn reduced feeding (Satapornvanit-09)')
+    lines(seq(0,900,10), sapply(seq(0,900,10),psi_q_zinc_satapornvanit09, simplify = T)[1,],
+          lty = 2, col = 2)
+    lines(seq(0,900,10), sapply(seq(0,900,10),psi_q_zinc_satapornvanit09, simplify = T)[2,],
+          lty = 3, col = 2)
+    lines(seq(0,900,10), sapply(seq(0,900,10),psi_q_zinc_satapornvanit09, simplify = T)[3,],
+          lty = 3, col = 2)  
+    
+  par.tricksz = c(coef(zinc.fr), 'Upper Limit:(Intercept)' = max(fr.z$feed_rate))[c(1,3,2)]
+    
+  psi_q_zinc_satapornvanit09_uncertainty<-function(In){
+    rdrm(nosim = 1, fct = LL.3(), mpar = par.tricksz, yerror = 'rnorm', xerror = In,
+         ypar = c(0, predict(zinc.fr, data.frame(dose = In), se.fit = T)[2]))$y / fr.z$feed_rate[1]
+  }
+    
+    points(seq(0,900,10), sapply(seq(0,900,10), psi_q_zinc_satapornvanit09_uncertainty, simplify = T), 
+           pch=5, col=4, cex = 0.5)   
+    
 #Chlorpyrifos ###################
   chlor.fr= drm(feed_rate ~ conc, data = fr.ch, type = 'continuous',
                fct = LL.4(names = c("Slope", "Lower Limit", "Upper Limit", "ED50"),
                           fixed = c(NA, 0, max(fr.ch$feed_rate), NA)))
     summary(chlor.fr)
-    plot(chlor.fr)
-  
+
   psi_q_chlor_satapornvanit09<-function(In){
-    predict(chlor.fr, data.frame(conc = In)) / predict(chlor.fr, data.frame(conc = 0))
+    predict(chlor.fr, data.frame(conc = In), interval = 'confidence', level = 0.95) / fr.ch$feed_rate[1]
   }  
   
   plot(fr.ch$conc, fr.ch$feed_rate / fr.ch$feed_rate[1], pch = 16, ylim = c(0,1),
-       xlab = 'Chlorpyrifos concentration (ppb)', ylab = 'Feeding rate (prey/prawn/hr)', main = 'Prawn reduced feeding (Sata-09)')
-  
-  fr.ch.df = data.frame(conc = seq(0,6,0.01),
-                       Prediction = 0,
-                       Lower = 0,
-                       Upper = 0)
-  
-  fr.ch.df[,2:4] <- predict(chlor.fr, newdata = fr.ch.df, 
-                           interval = 'confidence', level = 0.95)
-  
-  
-  lines(fr.ch.df$conc, fr.ch.df$Prediction / fr.ch.df$Prediction[1], col = 2, lty=2)
-  lines(fr.ch.df$conc, fr.ch.df$Lower / fr.ch.df$Prediction[1], col = 2, lty=3)
-  lines(fr.ch.df$conc, fr.ch.df$Upper / fr.ch.df$Prediction[1], col = 2, lty=3)  
+       xlab = 'Chlorpyrifos concentration (ppb)', ylab = 'Feeding rate (prey/prawn/hr)', 
+       main = 'Prawn reduced feeding (Sata-09)')
+    lines(seq(0,5,0.01), sapply(seq(0,5,0.01), psi_q_chlor_satapornvanit09, simplify = T)[1,],
+          lty = 2, col = 2)
+    lines(seq(0,5,0.01), sapply(seq(0,5,0.01), psi_q_chlor_satapornvanit09, simplify = T)[2,],
+          lty = 3, col = 2)
+    lines(seq(0,5,0.01), sapply(seq(0,5,0.01), psi_q_chlor_satapornvanit09, simplify = T)[3,],
+          lty = 3, col = 2)
+    
+  par.tricksc = c(coef(chlor.fr), 'Upper Limit:(Intercept)' = max(fr.ch$feed_rate))[c(1,3,2)]
+    
+    psi_q_chlor_satapornvanit09_uncertainty<-function(In){
+      rdrm(nosim = 1, fct = LL.3(), mpar = par.tricksc, yerror = 'rnorm', xerror = In,
+           ypar = c(0, predict(chlor.fr, data.frame(dose = In), se.fit = T)[2]))$y / fr.ch$feed_rate[1]
+    }
+    
+    points(seq(0,5,0.05), sapply(seq(0,5,0.05), psi_q_chlor_satapornvanit09_uncertainty, simplify = T), 
+           pch=5, col=4, cex = 0.5)   
     
 #*Note: checked that this produced the desired relationship on predator feeding rate and it does
