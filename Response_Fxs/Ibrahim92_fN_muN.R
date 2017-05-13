aa84364119fb99eed0a85597de1a5cfad2e7e65e
cmhoove14 <cmhoove14@gmail.com>
2017-05-12 17:02:25 -0700

    tweaks to normalize mortality to 0

--- Response_Fxs/Ibrahim92_fN_muN.R
+++ Response_Fxs/Ibrahim92_fN_muN.R
@@ -11,7 +11,10 @@
 
 #Toxicity to Biomphalaria snails from Ibrahim 1992 ###################
 require(drc)
-# reduction in snail fecundity: from table 1, relative change in number of juveniles produced over the 8 week experiment
+require(LW1949)
+
+# reduction in snail fecundity: from table 1, relative change in number of juveniles ####
+#produced over the 8 week experiment
 snail.repro = data.frame(dose = c(0,125,250,500), #chlorpyrifos in ppb
                          juvs = c(4225, 3005, 1749, 1313),
                         #convert 12 week juvenile cohort to juveniles per snail per day
@@ -42,22 +45,21 @@ f_N_chlor_ibr92 = function(In){
     if(In == 0) fn = 1 else{
       init = predict(chlor.fN.predict, newdata = data.frame(mal = In), se.fit = T)
       fn = rnorm(1, init[1], init[2]) / snail.repro$juvs.sn.day[1]
-      while(1 <= fn || fn <= 0){
-        fn = fn
+    }
+    while(1 < fn || fn < 0){
         fn = rnorm(1, init[1], init[2]) / snail.repro$juvs.sn.day[1]
-      }
     }
     return(fn)
   }
   
-    points(seq(0, 1000, 2), sapply(seq(0, 1000, 2), f_N_chlor_ibr92_uncertainty, simplify = T),
+    points(seq(0, 1000, 2), sapply(seq(0, 1000, 2), f_N_chlor_ibr92_uncertainty),
                pch = 5, cex = 0.6, col = 4)
     title(main = expression(paste('Ibrahim1992 - Chlorpyrifos reproductive toxicity to ',
                                   italic('Bi. alexandrina', sep = ''))))
       legend('topright', pch = c(16, 5), legend = c('Obs. points', 'Est. points'), col = c(1,4),
              cex = 0.7)
 
-    
+keep.ibr.fn.ch = c('snail.repro', 'chlor.fN.predict', 'f_N_chlor_ibr92_uncertainty')    
 #Snail mortality #############
   #-- relative change in survival over entire experiment period (12 weeks)
   #Not advised to use this as mortality estimate as outcomes were only assessed at 5 weeks.
@@ -72,34 +74,43 @@ f_N_chlor_ibr92 = function(In){
   
   ibr_muNq<-drm(dead/total ~ dose, weights = total, data = snail.mort,
                 type = 'binomial', fct = L.4(names = c('b', 'd', 'c', 'e'),
-                                              fixed = c(NA, 0, 1, NA)))
+                                              fixed = c(NA, snail.mort$mort[1], 1, NA)))
   
     summary(ibr_muNq)
 
-  plot(snail.mort$dose, snail.mort$mort, pch = 16, ylim = c(0,1))  
+  plot(snail.mort$dose, snail.mort$mort - snail.mort$mort[1], pch = 16, ylim = c(0,1),
+       xlab = 'Chlorpyrifos (ppb)', ylab = 'relative mortality')  
   
   mu_N_chlor_ibr92 = function(In){
       predict(ibr_muNq, data.frame(dose = In), interval = 'confidence', level = 0.95)
   }
   
-    lines(seq(0, 1000, 10), sapply(seq(0, 1000, 10), mu_N_chlor_ibr92, simplify = T)[1,],
-            lty = 2, col = 2)
-    lines(seq(0, 1000, 10), sapply(seq(0, 1000, 10), mu_N_chlor_ibr92, simplify = T)[2,],
-            lty = 3, col = 2)
-    lines(seq(0, 1000, 10), sapply(seq(0, 1000, 10), mu_N_chlor_ibr92, simplify = T)[3,],
-            lty = 3, col = 2)
+    lines(seq(0, 1000, 10), 
+          sapply(seq(0, 1000, 10), mu_N_chlor_ibr92, simplify = T)[1,] - snail.mort$mort[1],
+          lty = 2, col = 2)
+    lines(seq(0, 1000, 10), 
+          sapply(seq(0, 1000, 10), mu_N_chlor_ibr92, simplify = T)[2,] - snail.mort$mort[1],
+          lty = 3, col = 2)
+    lines(seq(0, 1000, 10), 
+          sapply(seq(0, 1000, 10), mu_N_chlor_ibr92, simplify = T)[3,] - snail.mort$mort[1],
+          lty = 3, col = 2)
   
 par.tricks.ibr.muN = c(coef(ibr_muNq), 
                        'Upper Limit:(Intercept)' = 1, 
                        'Lower Limit:(Intercept)' = snail.mort$mort[1])[c(1,4,3,2)]  
     
   mu_N_chlor_ibr92_uncertainty<-function(In){
-    rdrm(nosim = 1, fct = L.4(), mpar = par.tricks.ibr.muN, yerror = 'rbinom', xerror = In,
-         ypar = 30)$y / 30
+    mun = rdrm(nosim = 1, fct = L.4(), mpar = par.tricks.ibr.muN, yerror = 'rbinom', xerror = In,
+               ypar = 30)$y / 30 - snail.mort$mort[1]
+    while(mun < 0){
+      mun = rdrm(nosim = 1, fct = L.4(), mpar = par.tricks.ibr.muN, yerror = 'rbinom', xerror = In,
+                 ypar = 30)$y / 30 - snail.mort$mort[1]
+    }
+    return(mun)
   }
   
     points(seq(0, 1000, 2), sapply(seq(0, 1000, 2), mu_N_chlor_ibr92_uncertainty, simplify = T),
-               pch = 5, cex = 0.6, col = 4)
+               pch = 5, cex = 0.5, col = 4)
     title(main = expression(paste('Ibrahim1992 - Chlorpyrifos direct snail toxicity ',
                                   italic('Bi. alexandrina', sep = ''))))
       legend('bottomright', pch = c(16, 5), legend = c('Obs. points', 'Est. points'), col = c(1,4),
