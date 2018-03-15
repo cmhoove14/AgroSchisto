b4844d50e50d756c8ca9f3e88f330fce7412cda7
Chris Hoover <cmhoove14@gmail.com>
2018-03-15 09:47:25 -0700

    Updated code for plotting epsilon surface over transmission intensity and PDD

--- Elimination_Feasibility/Organize/Models/eps_surface_kap_lam.R
+++ Elimination_Feasibility/Organize/Models/eps_surface_kap_lam.R
@@ -18,57 +18,18 @@ require(rootSolve)
 
 no.cores = detectCores() - 1
 
-source("C:/Users/chris_hoover/Documents/RemaisWork/Schisto/Monash/Reff_fn_lib_CH.R")
-source("C:/Users/chris_hoover/Documents/RemaisWork/Schisto/Monash/lib_schistoModels_DDandNODD_CH.R")
+source("Elimination_Feasibility/Organize/Models/Reff_BBR_fns.R")
+source("Elimination_Feasibility/Organize/Models/schisto_mods_pdd_nopdd.R")
 
 #Use simplified r0 expression to determine range of lamda to test #########
-parameters_2pops_mda_Chris1["beta"]<-1.63e-6      #from best fit params with low transmission
-cov = 0.8
-parameters_2pops_mda_Chris1["cov"] = cov          # 80% coverage
-params<-parameters_2pops_mda_Chris1
-
-
-get_Ro_lamda<-function(lamda) #variable params to be sampled
-{ 
-  f_N<-params["f_N"]
-  phi_N<-params["phi_N"]
-  z<-params["z"]
-  mu_N<-params["mu_N"]
-  sigma<-params["sigma"]
-  mu_I<-params["mu_I"]
-  mu_W<-params["mu_W"]
-  m<-params["m"]
-  H<-params["H"]
-  mu_H<-params["mu_H"]
-  beta<-params["beta"]
-  
-  
-  #Equilibrium estimate of N given snail params
-  N_eq = max(uniroot.all(f = function(y){(f_N)*(1-y/phi_N) - mu_N
-  }, 
-  c(0, as.numeric(phi_N))))
-  
-  if(N_eq < 0){
-    N_eq = 0
-  }
-  
-  Ro_est <- sqrt((0.5*beta*H*N_eq*lamda*sigma)/((mu_W+mu_H)*(mu_N+sigma)*(mu_N+mu_I)))
-  
-  Ro_est
-  
-}
-
-init.lam = seq(1e-5,4e-4, length.out = 100)
-
-windows()
-plot(init.lam, sapply(init.lam, get_Ro_lamda, simplify = T), pch = 16, cex = 0.7)
-
+params<-pars_Chris1
+covrg <- pars_Chris1["cov"]
 #we'll go with a range of lambdas from 1e-4 to 3e-4 corresponding to r0 ~1-2
 
 #parameter ranges, values and vectors #########
   sim.range = 50                                    #number of values within test range to simulate
   lam.range = seq(1e-4, 3e-4, length.out = sim.range)  #transmission intensity
-  kap.range = seq(0, 2, length.out = sim.range)            #Pos. density dependence
+  kap.range = seq(1e-5, 2, length.out = sim.range)            #Pos. density dependence
 
 #temp vectors to fill
   w.pre = as.numeric()
@@ -76,7 +37,7 @@ plot(init.lam, sapply(init.lam, get_Ro_lamda, simplify = T), pch = 16, cex = 0.7
   bbr = as.numeric()
 
 #events data frame for MDA introduction (will be the same every time) #######
-  eff = 0.99                                        # 99% efficacy
+  eff = 0.94                                        # 94% efficacy
   mda.years = c(1:20) # annual MDA for 20 years
   
   mda.events = data.frame(var = rep('Wt', length(mda.years)),
@@ -94,7 +55,7 @@ eps.est = function(lam, kap){
   params["lamda"] = lam 
   params['k'] = kap
 
-  output<-as.data.frame(ode(nstart,teq,schisto_halstead_2pops_mda,params))
+  output<-as.data.frame(ode(nstart,teq,schisto_mod_pdd_add_ndds,params))
   
   eqbm2 = c(S = output[dim(output)[1], 2], 
             E = output[dim(output)[1], 3], 
@@ -102,12 +63,12 @@ eps.est = function(lam, kap){
             Wt = output[dim(output)[1],5], 
             Wu = output[dim(output)[1],6])
   
-  mda.sim = as.matrix(ode(eqbm2, time, schisto_halstead_2pops_mda, params,
+  mda.sim = as.matrix(ode(eqbm2, time, schisto_mod_pdd_add_ndds, params,
                       events = list(data = mda.events)))
   
   for(y in mda.years){
-    w.pre[y] = cov*mda.sim[(y*365), 5] + (1-cov) * mda.sim[(y*365), 6]
-    w.pos[y] = cov*mda.sim[(y*365+1), 5] + (1-cov) * mda.sim[(y*365+1), 6]
+    w.pre[y] = covrg*mda.sim[(y*365), 5] + (1-covrg) * mda.sim[(y*365), 6]
+    w.pos[y] = covrg*mda.sim[(y*365+1), 5] + (1-covrg) * mda.sim[(y*365+1), 6]
   }
   
   for(b in mda.years[-20]){
@@ -125,8 +86,8 @@ eps.fill = matrix(ncol = sim.range, nrow = sim.range)    #matrix to fill with ep
 clusteps = makeCluster(no.cores)
 clusterExport(cl = clusteps, 
               varlist = c('params','sim.range', 'lam.range', 'kap.range', 'ode', 'mda.years',
-                          'mda.events', 'nstart', 'teq', 'schisto_halstead_2pops_mda',
-                          'time', 'cov', 'w.pre', 'w.pos', 'bbr'))  
+                          'mda.events', 'nstart', 'teq', 'schisto_mod_pdd_add_ndds',
+                          'time', 'covrg', 'w.pre', 'w.pos', 'bbr', 'f_Wgk', 'R_Wv', 'phi_Wk'))  
 
 for(l in 1:sim.range){
   eps.fill[l,] = parSapply(clusteps, lam.range, eps.est, kap = kap.range[l], simplify = T)
@@ -134,6 +95,8 @@ for(l in 1:sim.range){
 } #lambda varies across a row; kappa varies down a column
 
 stopCluster(clusteps)
+
+save(eps.fill, file = "Elimination_Feasibility/Organize/Models/Outputs_Refs/eps_surface_lamRnage_kRange.Rdata")
 
 windows()
 
