370384f350f26d6e79e3887b9864a833ee95c57e
Chris Hoover <cmhoove14@gmail.com>
2018-06-26 14:46:29 -0700

    ggplot theme for lancet to use in figures

---
+++ Agrochemical_Review/Sims/ggplot_theme.R
@@ -0,0 +1,17 @@
+require(extrafont)
+
+loadfonts(device="win")
+
+theme_ms <- function(base_size=12, base_family="Times New Roman") {
+  library(grid)
+  (theme_bw(base_size = base_size, base_family = base_family)+
+      theme(text=element_text(color="black"),
+            axis.title=element_text(face="bold", size = rel(1.3)),
+            axis.text=element_text(size = rel(1), color = "black"),
+            legend.title=element_text(face="bold"),
+            legend.text=element_text(face="bold"),
+            legend.background=element_rect(fill="transparent"),
+            legend.key.size = unit(0.8, 'lines'),
+            panel.border=element_rect(color="black",size=1)
+    ))
+}
