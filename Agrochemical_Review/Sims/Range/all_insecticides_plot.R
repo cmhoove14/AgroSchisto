e996e9aad5625cc312126fa0f950c513bb8bcc21
Chris Hoover <cmhoove14@gmail.com>
2018-06-27 16:33:52 -0700

    Minor updates

--- Agrochemical_Review/Sims/Range/all_insecticides_plot.R
+++ Agrochemical_Review/Sims/Range/all_insecticides_plot.R
@@ -5,6 +5,9 @@ library(tidyverse)
 #GGplot theme for manuscripts
 source("Agrochemical_Review/Sims/ggplot_theme.R")
 
+#load R0 function
+source("Agrochemical_Review/Models/r0_of_q.R")
+
 #Load profenofos r0 simulations and nawqa data
 load("Agrochemical_Review/Sims/Range/Profenofos/prof_range.RData")
 load("Agrochemical_Review/Sims/Range/Profenofos/prof_r0_sims.RData")
@@ -39,10 +42,10 @@ load("Agrochemical_Review/Sims/Range/Chlorpyrifos/chlor_r0_sims.RData")
     filter(EEC <= 1)
 
 # All insecticides together
-rbind(prof_sims, mal_sims, chlor_sims) %>% 
+ins_plot <- rbind(prof_sims, mal_sims, chlor_sims) %>% 
   ggplot(aes(x = EEC, y = r0_smooth, col = Insecticide)) + 
   geom_line(size = 1) + #geom_vline(xintercept = log(prof_eec), lty = 2) +
-  geom_ribbon(aes(x = EEC, ymin = r025_smooth, ymax = r075_smooth, fill = Insecticide), alpha = 0.4) + 
+  geom_ribbon(aes(x = EEC, ymin = r025_smooth, ymax = r075_smooth, fill = Insecticide), alpha = 0.25) + 
   theme_ms() + ylab(expression(R[0])) + xlab("Agrochemical Concentration Normalized to Peak EEC") +
   ggtitle(expression(paste("Combined effects of key insecticides on estimates of ", R[0]))) +
   scale_x_continuous(breaks = seq(0,1,0.5),
@@ -50,4 +53,7 @@ rbind(prof_sims, mal_sims, chlor_sims) %>%
                      limits = c(0, 1.0001)) +
   scale_y_continuous(breaks = seq(1.25, 3.75, .5),
                      labels = seq(1.25, 3.75, .5),
-                     limits = c(1, 3.75))
+                     limits = c(1, 3.75)) +
+  geom_hline(yintercept = r0.fix()[3], lty = 2)
+
+ins_plot
