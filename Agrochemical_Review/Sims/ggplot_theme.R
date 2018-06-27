2df6c991f7d0ed9c31dc1377f58d8092977ed851
Chris Hoover <cmhoove14@gmail.com>
2018-06-27 16:32:33 -0700

    add elements to ggplot theme

--- Agrochemical_Review/Sims/ggplot_theme.R
+++ Agrochemical_Review/Sims/ggplot_theme.R
@@ -6,7 +6,9 @@ theme_ms <- function(base_size=12, base_family="Times New Roman") {
   library(grid)
   (theme_bw(base_size = base_size, base_family = base_family)+
       theme(text=element_text(color="black"),
-            plot.title = element_text(face="bold", size = rel(1.1)),
+            plot.title = element_text(face="bold", size = rel(1.25)),
+            strip.background = element_blank(),
+            strip.text.y = element_text(face = "bold", size = rel(1.1)),
             axis.title=element_text(size = rel(1.1)),
             axis.text=element_text(size = rel(1), color = "black"),
             legend.title=element_text(face="bold"),
