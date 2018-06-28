e1b2eee9015937f2d4e1311472c8820bd10f1f29
Chris Hoover <cmhoove14@gmail.com>
2018-06-28 14:19:26 -0700

    Format to match lancet figure requirements

--- Agrochemical_Review/Sims/ggplot_theme.R
+++ Agrochemical_Review/Sims/ggplot_theme.R
@@ -1,15 +1,13 @@
 require(extrafont)
 
-loadfonts(device="win")
-
-theme_ms <- function(base_size=12, base_family="Times New Roman") {
+theme_ms <- function(base_size=10, base_family="Times New Roman") {
   library(grid)
   (theme_bw(base_size = base_size, base_family = base_family)+
       theme(text=element_text(color="black"),
-            plot.title = element_text(face="bold", size = rel(1.25)),
+            plot.title = element_text(face="bold"),
             strip.background = element_blank(),
-            strip.text.y = element_text(face = "bold", size = rel(1.1)),
-            axis.title=element_text(size = rel(1.1)),
+            strip.text.y = element_text(face = "bold", size = rel(1)),
+            axis.title=element_text(size = rel(1)),
             axis.text=element_text(size = rel(1), color = "black"),
             legend.title=element_text(face="bold"),
             #legend.text=element_text(face="bold"),
