85dd589e645e1bd94265e3a86ee826a48b15c8a9
Chris Hoover <cmhoove14@gmail.com>
2019-04-29 22:14:18 -0700

    Take out R0 generation so as just to produce parameter sets, keep parameter set generation and simulations separate

--- Agrochemical_Review/Sims/EEC/generate_eec_parameter_sets.R
+++ Agrochemical_Review/Sims/EEC/generate_eec_parameter_sets.R
@@ -12,14 +12,20 @@ pars_df <- as.data.frame(as.list(fit_pars))
 #Get csv file containing summary of all response functions
 RFxSum <- read_csv("Agrochemical_Review/Response_Fxs/Summary/Response_Fx_Summary.csv")
 
+RFxSum <- RFxSum %>% 
+  arrange(Study) %>% 
+  group_by(Study) %>% 
+  mutate(ID = row_number(),
+         Study_ID_Unique = paste0(Study, "_", ID)) %>% ungroup()
+
 #Source all .R scripts that contain derivation of response functions
 RFx_files <- paste0("Agrochemical_Review/Response_Fxs/", 
                     list.files("Agrochemical_Review/Response_Fxs/")[grepl(".R", list.files("Agrochemical_Review/Response_Fxs/"))])
 
 sapply(RFx_files, source)
 
-#Function to generate parameter sets to simulate agrochemical effects at EEC for each response function, then apply the r0 function to every realization of the parameter set and save the resulting dataframe
-gen_eec_par_set <- function(eec, nsims, rfx, par, out_name){
+#Function to generate parameter sets to simulate agrochemical effects at EEC for each response function
+gen_eec_par_set <- function(eec, nsims, rfx, par, study){
   
   #create nsims duplicate rows of base parameters
   par_set <- pars_df %>% slice(rep(1:n(), each = nsims))
@@ -31,32 +37,45 @@ gen_eec_par_set <- function(eec, nsims, rfx, par, out_name){
   #Replace affected agrochemical parameter with estimates from rfx
   par_set[par] <- sapply(rep(eec, nsims), rfx)
   
-  #Apply r0 function to parameter set
-  r0s <- pmap_df(par_set, r0.Ag.pars)
-  
-  #bind parameters and rsults together
-  r0_out <- cbind(par_set, r0s)
-  
-  r0_out["Study"] <- out_name
+  par_set["Study_ID_Unique"] <- study
 
   #Save data frame of simulation parameter set
-  save(r0_out, file = paste0("Agrochemical_Review/Sims/EEC/Parameter_Sets/", out_name, "_eec_parameters_r0s.Rdata"))
+  saveRDS(par_set, file = paste0("Agrochemical_Review/Sims/EEC/Parameter_Sets/", 
+                                 study, 
+                                 "_eec_parameters.rds"))
   
 }
 
 #Do this for all response functions with relevant parameters affected
-RFxSum_Sub <- RFxSum %>% filter(parameter %in% colnames(pars_df) & !is.na(eec) & Class != "Fertilizer")
+RFxSum_Sub <- RFxSum %>% filter(parameter %in% colnames(pars_df) & !is.na(rfx))
  
+num_sims = 1000
+
 set.seed(43093)
 
 for(i in 1:nrow(RFxSum_Sub)){
-  gen_eec_par_set(RFxSum_Sub$eec[i], 1000, 
-                  RFxSum_Sub$rfx[i], RFxSum_Sub$parameter[i], 
-                  paste(RFxSum_Sub$Study[i], RFxSum_Sub$Chemical[i], sep = "_"))
+  gen_eec_par_set(eec = RFxSum_Sub$eec[i], 
+                  nsims = num_sims, 
+                  rfx = RFxSum_Sub$rfx[i], 
+                  par = RFxSum_Sub$parameter[i],
+                  study = RFxSum_Sub$Study_ID_Unique[i])
   
   if(i %% 20 == 0) print(i)
 } 
 
-#Check that each unique response function has a unique parameter set
+#Also add dataset with no response function for baseline compariosn ############
+  #create nsims duplicate rows of base parameters
+  par_set_base <- pars_df %>% slice(rep(1:n(), each = num_sims))
+  
+  #Replace lambda transmission parameter with sample from vals within 95%CI
+  par_set_base["lambda"] <- sample(fit_1par_mat$lambda_1par, num_sims, 
+                                   replace = T, prob = fit_1par_mat$weight)
+
+  par_set_base["Study_ID_Unique"] <- "baseline_eec_parameters"
+
+  #Save data frame of simulation parameter set
+  saveRDS(par_set_base, file = paste0("Agrochemical_Review/Sims/EEC/Parameter_Sets/", "baseline_eec_parameters.rds"))
+
+#Check that each unique response function has a unique parameter set ###########
 #Right now, this is not true. Need to update naming convention
-nrow(RFxSum_Sub) == length(list.files("Agrochemical_Review/Sims/EEC/Parameter_Sets/"))
+nrow(RFxSum_Sub) == length(list.files("Agrochemical_Review/Sims/EEC/Parameter_Sets/"))-1
