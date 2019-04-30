fbccd85d0e51c3c98ebffe178032dc19f707d1d5
Chris Hoover <cmhoove14@gmail.com>
2019-04-29 22:13:37 -0700

    Script to generate forestplot in ggplot from R0 simulations at every agrochemical's EEC

---
+++ Agrochemical_Review/Sims/EEC/forestplot_ggplot.R
@@ -0,0 +1,75 @@
+require(rootSolve)
+require(tidyverse)
+
+source("Agrochemical_Review/Models/r0_functions.R")
+source("Agrochemical_Review/Models/model_helper_functions.R")
+source("Agrochemical_Review/Models/initial_parameters.R")
+
+RFxSum <- read_csv("Agrochemical_Review/Response_Fxs/Summary/Response_Fx_Summary.csv") %>% 
+  arrange(Study) %>% 
+  group_by(Study) %>% 
+  mutate(ID = row_number(),
+         Study_ID_Unique = paste0(Study, "_", ID)) %>% ungroup()
+
+all_eec_pars <- 
+  do.call(rbind,
+          lapply(paste0("Agrochemical_Review/Sims/EEC/Parameter_Sets/",
+                       list.files(path = "Agrochemical_Review/Sims/EEC/Parameter_Sets/")), readRDS))
+
+#Apply r0 function to all parameter sets
+  r0s <- pmap_df(all_eec_pars %>% dplyr::select(-Study_ID_Unique), r0.Ag.pars)
+  
+#bind parameters and results together
+  all_eec_sims <- cbind(all_eec_pars, r0s) %>% 
+    left_join(RFxSum)
+
+#Separate baseline sims from all other sims
+  baseline_eec_sims <- all_eec_sims %>% filter(Study_ID_Unique == "baseline_eec_parameters")
+  
+#Baseline summary statistics  
+  base_r0_median <- median(baseline_eec_sims$R0)
+  base_r0_q25 <- quantile(baseline_eec_sims$R0, 0.25)
+  base_r0_q75 <- quantile(baseline_eec_sims$R0, 0.75)
+
+  base_r0_q025 <- quantile(baseline_eec_sims$R0, 0.025)
+  base_r0_q975 <- quantile(baseline_eec_sims$R0, 0.975)
+
+#All other sims  
+  eec_sims <- all_eec_sims %>% filter(Study_ID_Unique != "baseline_eec_parameters")
+  
+#Ggplot violin forestplot figure  #######
+  
+  eec_sims %>% 
+    ggplot(aes(y = R0, x = Study_ID_Unique, fill = Pathway)) + 
+      geom_rect(ymin = base_r0_q025, ymax = base_r0_q975,
+                xmin = 0, xmax = nrow(eec_sims), alpha = 0.2, fill = "grey80") +
+      geom_hline(yintercept = base_r0_median, size = 1.2) +
+      geom_violin(alpha = 0.8) +
+      scale_fill_manual(values = c("bottom-up" = "#42B54099",
+                                   "direct larvae" = "#925E9F99",
+                                   "direct snail" = "#0099B499",
+                                   "top-down" = "#ED000099")) +
+      coord_flip() +
+      facet_grid(Class~., scales = "free", space = "free") +
+      theme_classic()
+  
+#Ggplot classic forestplot figure  #######
+  eec_sims %>% 
+    group_by(Study_ID_Unique) %>% 
+    summarise(med_R0 = median(R0),
+              R0_25 = quantile(R0, 0.25),
+              R0_75 = quantile(R0, 0.75),
+              Pathway = first(Pathway),
+              Class = first(Class)) %>% 
+    ggplot(aes(y = Study_ID_Unique, x = med_R0, xmin = R0_25, xmax = R0_75, col = Pathway)) + 
+      geom_rect(xmin = base_r0_q25, xmax = base_r0_q75,
+                ymin = 0, ymax = nrow(eec_sims), alpha = 0.2, fill = "grey80") +
+      geom_vline(xintercept = base_r0_median, size = 1.2) +
+      geom_point() +
+      geom_errorbarh(height = 0.2) +
+      scale_color_manual(values = c("bottom-up" = "#42B54099",
+                                    "direct larvae" = "#925E9F99",
+                                    "direct snail" = "#0099B499",
+                                    "top-down" = "#ED000099")) +
+      facet_grid(Class~., scales = "free", space = "free") +
+      theme_classic()
